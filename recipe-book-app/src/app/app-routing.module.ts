e07b8260aa3177804ffc32eebab193d3f5b9544b
karinplaninsek <karin.planinsek@gmail.com>
2020-11-14 21:21:34 +0100

    Creating search-recipe component and adding basic html template

--- recipe-book-app/src/app/app-routing.module.ts
+++ recipe-book-app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SearchRecipeComponent } from './search-recipe/search-recipe.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchRecipeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- recipe-book-app/src/app/app.module.ts
+++ recipe-book-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchRecipeComponent } from './search-recipe/search-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchRecipeComponent
   ],
   imports: [
     BrowserModule,
